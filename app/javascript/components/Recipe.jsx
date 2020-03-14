fd302c544c711ef8f48a0b6b48508b58438d9636
Sofia Canclini <canclini@kth.se>
2020-03-14 09:03:45 +0100

    delete recipe

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -35,6 +35,32 @@ class Recipe extends React.Component {
       .replace(/&gt;/g, ">");
   }
 
+  deleteRecipe = () => {
+    const {
+      match: {
+        params: { id }
+      }
+    } = this.props;
+    const url = `/api/v1/destroy/${id}`;
+    const token = document.querySelector('meta[name="csrf-token"]').content;
+
+    fetch(url, {
+      method: "DELETE",
+      headers: {
+        "X-CSRF-Token": token,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then(() => this.props.history.push("/recipes"))
+      .catch(error => console.log(error.message));
+  };
+
   render() {
     const { recipe } = this.state;
     let ingredientList = "No ingredients available";
@@ -80,7 +106,11 @@ class Recipe extends React.Component {
               />
             </div>
             <div className="col-sm-12 col-lg-2">
-              <button type="button" className="btn btn-danger">
+              <button
+                type="button"
+                className="btn btn-danger"
+                onClick={this.deleteRecipe}
+              >
                 Delete Recipe
               </button>
             </div>
